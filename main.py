7db0df0f08bcffe88126aa771e40dfc5d9b2810c
nirajdhakal <yboii34@gmail.com>
2023-03-31 16:46:42 -0400

    in progress...

--- main.py
+++ main.py
@@ -2,9 +2,26 @@ def computer(userInput):
     userInput = userInput.split()
     programMemory = []
     randomMemory = []
+    haltString = ['hlt','HLT']
+    movString = ['mov','MOV']
+    addString = ['add','ADD']
+    subString = ['sub','SUB']
+    mulString = ['mul','MUL']
+    divString = ['div','DIV']
+    modString = ['mod','MOD']
+    jmpString = ['jmp','JMP']
+    cmpString = ['cmp','CMP']
+    jlString = ['jl','JL']
+    jgString = ['jg','JG']
+    jeString = ['je','JE']
+    jneString = ['jne','JNE']
     for x in range(int(userInput[1])):
         randomMemory.append("0")
     print(randomMemory)
+    infile = open(userInput[0],'r')
+    for j in range(len(randomMemory)):
+        line = infile.readline().split()
+        print(line)
 
 
 
