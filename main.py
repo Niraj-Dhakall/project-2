a88a8bfe45ea95319d49fffe7e3b501091b0b76b
nirajdhakal <yboii34@gmail.com>
2023-04-13 13:39:52 -0400

    in progress...

--- main.py
+++ main.py
@@ -12,15 +12,32 @@ def interrupt(userInput, memory):
         intterruptInput = int(input(''))
         inputIndex = strip(userInput[2],'[]')
         memory[int(inputIndex)] = intterruptInput
+
 def jump(userInput, memory, compareList):
     jlString = ['jl','JL']
     jgString = ['jg','JG']
     jeString = ['je','JE']
     jneString = ['jne','JNE']
-    if userInput[0] in jlString:
+    jgeString = ['jge','JGE']
+    jleString = ['jle','JLE']
+    if userInput[0] in jlString: # this is for jumping if less than. Returns 1 if values are less than each other
         if compareList[0]:
             return 1
-
+    if userInput[0] in jgString: # this is for jumping if greater than. Returns 1 if both less than and equal to are false
+        if not compareList[0] and not compareList[1]:
+            return 1
+    if userInput[0] in jeString: # this is for jumping if equal to. Returns 1 if the values are equal to each other
+        if compareList[1]:
+            return 1
+    if userInput[0] in jneString: # this is for jumping if not equal to. Returns 1 if the values are not equal to each other
+        if not compareList[1]:
+            return 1
+    if userInput[0] in jgeString: # this is is for jumping if greater than or equal to. Returns 1 if not less than or  equal to
+        if not compareList[0] or compareList[1]:
+            return 1
+    if userInput[0] in jleString: # this is for jumping if less than or equal to. Returns 1 if values are less than or equal to
+        if compareList[0] or compareList[1]:
+            return 1
 def operationCommands(userInput,memory):
     parameterOne = userInput[0]
     parameterTwo = userInput[1]
@@ -45,7 +62,7 @@ def operationCommands(userInput,memory):
             addedValue = int(parameterThree) + memory[locationTwo]
         elif(bracketString in parameterThree and bracketString not in parameterFour):
             locationOne = strip(parameterThree,'[]')
-            addedValue = parameterFour + memory[locationOne]
+            addedValue = int(parameterFour) + memory[locationOne]
         else:
             addedValue = parameterThree + parameterFour
     memory[strip(parameterTwo,'[]')] = addedValue
@@ -108,14 +125,20 @@ def computer(userInput):
     cmpString = ['cmp','CMP']
     intString = ['int','INT']
     instructionCounter = 0
+    stopReading = False
     for x in range(int(userInput[1])):
         randomMemory.append(0)
     infile = open(userInput[0],'r')
 
     line = infile.readline().split()
-    while line[0] not in haltString:
-        programMemory.append(line)
-        line = infile.readline().split()
+    while not stopReading:
+        if line[0] in haltString:
+            programMemory.append(line)
+            stopReading = True
+        else:
+            programMemory.append(line)
+            line = infile.readline().split()
+
     while programMemory[instructionCounter] not in haltString:
         if haltString not in programMemory[instructionCounter]:
             line = programMemory[instructionCounter]
@@ -131,7 +154,7 @@ def computer(userInput):
                 compareList = compareCommand(line,randomMemory)
                 if instructionCounter + 1 < len(programMemory):
                     instructionCounter+=1
-            elif(jmpString in programMemory[instructionCounter][0]):
+            elif(jmpString in programMemory[instructionCounter][0].lower()):
                 if jump(line,randomMemory,compareList) == 1:
                     jumpLine = programMemory[instructionCounter][1]
                     instructionCounter = int(jumpLine)
@@ -145,6 +168,6 @@ def computer(userInput):
 
 
 if __name__ == '__main__':
-    userInput = 'for_loop_2.ret 10'
+    userInput = 'fib.ret 20'
 
     computer(userInput)
